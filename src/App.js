c817e9b8ccb13ee50e8a93f6368b664da72f156a
xshandy <shandygshek@hotmail.co.uk>
2024-11-08 17:40:19 +0000

    i added git repository

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      Weather App
+     <footer>
+      This project was coded by Shandy Shek and is <a href="https://github.com/xshandy/weatherapp" target="blank">open-sourced on Github.</a>
+     </footer>
     </div>
   );
 }
