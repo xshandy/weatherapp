107340a6563c4077e8b0965afc7aaf10f3eab091
xshandy <shandygshek@hotmail.co.uk>
2024-11-10 15:43:52 +0000

    installed bootstrap and downgraded node.js as it was causing error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+       
      Weather App
+     <button className='btn btn-primary'>Bootstrap Button</button>
      <footer>
-      This project was coded by Shandy Shek and is <a href="https://github.com/xshandy/weatherapp" target="blank">open-sourced on Github.</a>
+      This project was coded by Shandy Shek and is <a href="https://github.com/xshandy/weatherapp" target="_blank" rel="noopener noreferrer">open-sourced on Github.</a>
      </footer>
+     </div>
     </div>
   );
 }
